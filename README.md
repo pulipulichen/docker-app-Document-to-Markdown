e361e17bf0452767c0d24b98a1825a26cce99ebe
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-01-10 13:53:06 +0800

    西元2025年01月10日 (週五) 13時53分06秒 CST

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# docker-app-Document-to-Markdown
-Converts documents to Markdown format.
+@TODO: DOI
+
+# docker-app-Document-to-Markdown: 將文件轉換成Markdown格式。
+
+這個Docker APP會將輸入的各式文件，例如Word、PDF等等，轉換成Markdown格式的檔案輸出。
+
+This Docker APP takes various document formats as input, such as Word and PDF files, and converts them into Markdown format.
+
+@TODO IMG
+
+# Download
+
+- For Windows: https://pulipulichen.github.io/docker-app-Document-to-Markdown/bin/doc-to-md.exe
+- For Linux and Mac OS: https://pulipulichen.github.io/docker-app-Document-to-Markdown/bin/doc-to-md.sh
+
+# Online Tool
+
+https://colab.research.google.com/github/pulipulichen/docker-app-Document-to-Markdown/blob/main/colab/docker-app-Document-to-Markdown.ipynb
+
+# Blog
+
+@TODO
+
+# Key Technologies
+
+- **Docker**: 提供容器化部署，確保應用程式的可移植性和管理便利性。
+- **MarkItDown**: 將文件轉換成markdown的套件。
+
+# Citation
+
+@TODO
---
+++ app/index.js
@@ -0,0 +1,45 @@
+const ShellSpawn = require('./lib/ShellSpawn')
+const ShellExec = require('./lib/ShellExec')
+const GetFiles = require('./lib/GetFiles')
+
+const path = require('path')
+const fs = require('fs')
+
+const isColab = require('./lib/isColab')
+
+let main = async function () {
+
+  if (isColab) {
+    await ShellSpawn(`rm -rf /output/*`)
+  }
+
+  let files = GetFiles()
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i]
+    
+    let filename = path.basename(file)
+    let dirname = path.dirname(file)
+    let filenameNoExt = path.parse(filename).name
+    let ext = path.extname(filename)
+    if (ext !== '.pdf' || filename.endsWith('-clean.pdf')) {
+      continue
+    }
+
+    let fileTmp = `/tmp/input.pdf`
+    if (fs.existsSync(fileTmp)) {
+      fs.unlinkSync(fileTmp)
+    }
+    await ShellExec(`cp "${file}" ${fileTmp}`)
+
+    let cleanPDFfileTmp = `/tmp/output.pdf`
+    if (fs.existsSync(cleanPDFfileTmp)) {
+      fs.unlinkSync(cleanPDFfileTmp)
+    }
+    await ShellExec(`pdftk "${fileTmp}" output - uncompress | sed '/^\\/Annots/d' | pdftk - output "${cleanPDFfileTmp}" compress`)
+
+    let cleanPDFfile = '/output/' + filenameNoExt + '-clean.pdf'
+    await ShellExec(`cp "${cleanPDFfileTmp}" "${cleanPDFfile}"`)
+  }
+}
+
+main()
