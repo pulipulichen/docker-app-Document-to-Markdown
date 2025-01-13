df094e995aa4edb3c1e0aa70122ee1afe2638a7a
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-01-13 13:26:54 +0800

    西元2025年01月13日 (週一) 13時26分54秒 CST

--- index.js
+++ index.js
@@ -1,70 +1,51 @@
-const ShellSpawn = require('./app/lib/ShellSpawn')
-const GetExistedArgv = require('./app/lib/GetExistedArgv')
-const SetDockerComposeYML = require('./app/lib/SetDockerComposeYML')
+const ShellSpawn = require('./lib/ShellSpawn')
+const ShellExec = require('./lib/ShellExec')
+const GetFiles = require('./lib/GetFiles')
 
-// const dialog = require('node-file-dialog')
-
-const fs = require('fs')
 const path = require('path')
+const fs = require('fs')
 
-const lockFile = path.resolve(__dirname, '.lock')
-let lock = function () {
-	fs.writeFileSync(lockFile, (new Date().toString()))
-}
-
-let isLocked = function () {
-	return fs.existsSync(lockFile)
-}
-
-let unlock = function () {
-	fs.unlinkSync(lockFile)
-}
-
-function sleep(ms = 500) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
+const isColab = require('./lib/isColab')
 
 let main = async function () {
-	while (isLocked()) {
-		await sleep()
-	}
-	lock()
 
-  // 1. 先取得輸入檔案的列表
-  let files = GetExistedArgv()
-
-  // console.log('gogogo', files)
-  for (let i = 0; i < files.length; i++) {
-    let file = files[i]
-
-		// ------------
-
-		// let filename = path.basename(file)
-		// let dirname = path.dirname(file)
-		// let tempFilename = Math.floor(Math.random() * 100000) + '.zip'
-		
-		// let tempFilePath = path.resolve(dirname, tempFilename)
-		// // console.log(tempFilePath)
-		// // console.log(path.resolve(dirname, filename.slice(0, -4) + '.txt'))
-
-		// fs.renameSync(file, tempFilePath)
-
-		// ------------
-    
-    // SetDockerComposeYML(tempFilePath)
-		SetDockerComposeYML(file)
-    await ShellSpawn('docker-compose up')
-
-		// --------------
-
-		// fs.renameSync(tempFilePath, file)
-		// if (fs.existsSync(path.resolve(dirname, tempFilename.slice(0, -4)) + '.pdf')) {
-		// 	fs.renameSync(path.resolve(dirname, tempFilename.slice(0, -4)) + '.pdf', path.resolve(dirname, filename.slice(0, -4)) + '.pdf')
-		// }
+  if (isColab) {
+    await ShellSpawn(`rm -rf /output/*`)
   }
 
-	unlock()
+  let files = GetFiles()
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i]
+    
+    let filename = path.basename(file)
+    // let dirname = path.dirname(file)
+    let filenameNoExt = path.parse(filename).name
+    let ext = path.extname(filename)
+    if (ext === '.md' || ext === '.txt') {
+      continue
+    }
 
+    // if (ext === '.pdf') {
+    //   let fileHTMLtmp = `/tmp/input.html`
+    //   await ShellExec(`pdftohtml "${file}" ${fileHTMLtmp}`)
+    //   file = fileHTMLtmp
+    // }
+
+    let fileTmp = `/tmp/input`
+    if (fs.existsSync(fileTmp)) {
+      fs.unlinkSync(fileTmp)
+    }
+    await ShellExec(`cp "${file}" ${fileTmp}`)
+
+    let fileOutputTmp = `/tmp/output`
+    if (fs.existsSync(fileOutputTmp)) {
+      fs.unlinkSync(fileOutputTmp)
+    }
+    await ShellExec(`markitdown "${fileTmp}" > ${fileOutputTmp}`)
+
+    let fileOutput = '/output/' + filenameNoExt + '.md'
+    await ShellExec(`cp "${fileOutputTmp}" "${fileOutput}"`)
+  }
 }
 
 main()
